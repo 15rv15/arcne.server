abd38863d54667c88b398b3cbfaeef7703e72f24
rvenugopal <rvenugopal086@student.fuhsd.org>
2025-05-04 12:11:31 -0700

    Initial commit

---
+++ src/main/java/io/arcne/GameServer.java
@@ -0,0 +1,56 @@
+package io.arcne;
+
+import org.java_websocket.server.WebSocketServer;
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+
+import java.net.InetSocketAddress;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class GameServer extends WebSocketServer {
+
+    private final Set<WebSocket> clients = Collections.synchronizedSet(new HashSet<>());
+
+    public GameServer(int port) {
+        super(new InetSocketAddress(port));
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        clients.add(conn);
+        System.out.println("Client connected: " + conn.getRemoteSocketAddress());
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        clients.remove(conn);
+        System.out.println("Client disconnected: " + conn.getRemoteSocketAddress());
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        // Broadcast incoming message to all connected clients
+        for (WebSocket client : clients) {
+            client.send(message);
+        }
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        System.out.println("WebSocket Error:");
+        ex.printStackTrace();
+    }
+
+    @Override
+    public void onStart() {
+        System.out.println("GameServer started on port " + getPort());
+    }
+
+    public static void main(String[] args) {
+        int port = 10000; // Change if needed
+        GameServer server = new GameServer(port);
+        server.start();
+    }
+}
